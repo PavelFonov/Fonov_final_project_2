593febdad157e8e36ed8bd4676ccfab7b41cdc5c
PavelFonov <ps.fonov@yandex.ru>
2024-05-01 10:37:41 +0300

    Project after 2 lections

---
+++ src/com/fonov/consts/Consts.java
@@ -0,0 +1,11 @@
+package com.fonov.consts;
+
+public class Consts {
+    private Consts(){
+    }
+    public static final String PATH_TO_ENTITY_CHARACTERISTICS = "entity_characteristic.json";
+    public static final String PATH_TO_POSSIBIBLITY = "resourses/possibility_of_eating.json";
+    public static final String PATH_TO_ISLAND_SETTINGS = "C:\\Users\\Admin\\Desktop\\J\\My Projects\\final_project_2\\final_project_2\\src\\resourses\\settings.properties";
+
+
+}
